5c568f2d21832218b4f3d9e7443561ddab34a51b
guMirandaa <127452811+guMirandaa@users.noreply.github.com>
2023-11-30 20:58:21 -0300

    Update resultado_query2_questao4_noSQL.js

--- resultados/resultado_query2_questao4_noSQL.js
+++ resultados/resultado_query2_questao4_noSQL.js
@@ -1,156 +1,25 @@
-[
-  {
-    _id: {
-      nome_aluno: 'Zhang',
-      nome_instrutor: 'Katz',
-      nome_curso: 'Image Processing'
-    }
-  },
-  {
-    _id: {
-      nome_aluno: 'Brown',
-      nome_instrutor: 'Katz',
-      nome_curso: 'Intro. to Computer Science'
-    }
-  },
-  {
-    _id: {
-      nome_aluno: 'Bourikas',
-      nome_instrutor: 'Kim',
-      nome_curso: 'Intro. to Digital Systems'
-    }
-  },
-  {
-    _id: {
-      nome_aluno: 'Zhang',
-      nome_instrutor: 'Katz',
-      nome_curso: 'Intro. to Computer Science'
-    }
-  },
-  {
-    _id: {
-      nome_aluno: 'Aoi',
-      nome_instrutor: 'Kim',
-      nome_curso: 'Intro. to Digital Systems'
-    }
-  },
-  {
-    _id: {
-      nome_aluno: 'Zhang',
-      nome_instrutor: 'Katz',
-      nome_curso: 'Game Design'
-    }
-  },
-  {
-    _id: {
-      nome_aluno: 'Shankar',
-      nome_instrutor: 'Srinivasan',
-      nome_curso: 'Game Design'
-    }
-  },
-  {
-    _id: {
-      nome_aluno: 'Shankar',
-      nome_instrutor: 'Srinivasan',
-      nome_curso: 'Image Processing'
-    }
-  },
-  {
-    _id: {
-      nome_aluno: 'Brown',
-      nome_instrutor: 'Katz',
-      nome_curso: 'Image Processing'
-    }
-  },
-  {
-    _id: {
-      nome_aluno: 'Brown',
-      nome_instrutor: 'Katz',
-      nome_curso: 'Game Design'
-    }
-  },
-  {
-    _id: {
-      nome_aluno: 'Shankar',
-      nome_instrutor: 'Srinivasan',
-      nome_curso: 'Robotics'
-    }
-  },
-  {
-    _id: {
-      nome_aluno: 'Brown',
-      nome_instrutor: 'Katz',
-      nome_curso: 'Database System Concepts'
-    }
-  },
-  {
-    _id: {
-      nome_aluno: 'Peltier',
-      nome_instrutor: 'Einstein',
-      nome_curso: 'Physical Principles'
-    }
-  },
-  {
-    _id: {
-      nome_aluno: 'Zhang',
-      nome_instrutor: 'Katz',
-      nome_curso: 'Robotics'
-    }
-  },
-  {
-    _id: {
-      nome_aluno: 'Shankar',
-      nome_instrutor: 'Srinivasan',
-      nome_curso: 'Database System Concepts'
-    }
-  },
-  {
-    _id: {
-      nome_aluno: 'Levy',
-      nome_instrutor: 'Einstein',
-      nome_curso: 'Physical Principles'
-    }
-  },
-  {
-    _id: {
-      nome_aluno: 'Tanaka',
-      nome_instrutor: 'Crick',
-      nome_curso: 'Genetics'
-    }
-  },
-  {
-    _id: {
-      nome_aluno: 'Zhang',
-      nome_instrutor: 'Katz',
-      nome_curso: 'Database System Concepts'
-    }
-  },
-  {
-    _id: {
-      nome_aluno: 'Chavez',
-      nome_instrutor: 'Singh',
-      nome_curso: 'Investment Banking'
-    }
-  },
-  {
-    _id: {
-      nome_aluno: 'Tanaka',
-      nome_instrutor: 'Crick',
-      nome_curso: 'Intro. to Biology'
-    }
-  },
-  {
-    _id: {
-      nome_aluno: 'Shankar',
-      nome_instrutor: 'Srinivasan',
-      nome_curso: 'Intro. to Computer Science'
-    }
-  },
-  {
-    _id: {
-      nome_aluno: 'Brown',
-      nome_instrutor: 'Katz',
-      nome_curso: 'Robotics'
-    }
-  }
-]
+{
+  cursorHasMore: false,
+  documents: [
+    { building: 'Packard', room_number: '101', professor: 'Mozart' },
+    {
+      building: 'Taylor',
+      room_number: '3128',
+      professor: 'Srinivasan'
+    },
+    { building: 'Painter', room_number: '514', professor: 'Crick' },
+    {
+      building: 'Packard',
+      room_number: '101',
+      professor: 'Srinivasan'
+    },
+    { building: 'Painter', room_number: '514', professor: 'El Said' },
+    { building: 'Watson', room_number: '100', professor: 'Brandt' },
+    { building: 'Taylor', room_number: '3128', professor: 'Brandt' },
+    { building: 'Taylor', room_number: '3128', professor: 'Kim' },
+    { building: 'Watson', room_number: '120', professor: 'Srinivasan' },
+    { building: 'Packard', room_number: '101', professor: 'Katz' },
+    { building: 'Watson', room_number: '100', professor: 'Einstein' },
+    { building: 'Packard', room_number: '101', professor: 'Wu' }
+  ]
+}
